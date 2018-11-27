57148fd5b98a916a864b497e924d396e01400937
Avel FERON PAPEL <36990281+magavel@users.noreply.github.com>
2018-11-27 07:44:57 +0100

    Création des fichiers

---
+++ README.md
@@ -0,0 +1,7 @@
+# Structure de référence
+
+
+Projet avec gulp
+
+
+Demarrer le projet avec npm install
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="css/mon_site.css">
+</head>
+<body>
+<div class="content">
+
+    <div id="nav">
+
+        <?php include_once ('menu.php');?>
+
+
+    </div> <!-- fin menu-->
+
+</div>
+
+</body>
+</html>
